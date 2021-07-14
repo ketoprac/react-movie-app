15dc8f9d1cad8af3f2d1da6a5631574d83108f5b
unknown <mada.lawliet@gmail.com>
2021-07-14 15:35:35 +0700

    fixed props

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const MovieList = ({movies, favouriteComponent, handleFavouritesClick}) => {
-  const FavouriteComponent = favouriteComponent;
+const MovieList = (props) => {
+  const FavouriteComponent = props.favouriteComponent;
   
   return (
     <>
-     {movies.map((movie, index) => (
+     {props.movies.map((movie, index) => (
      <div key={index} className="image-container">
        <img src={movie.Poster} alt="movie"></img>
         <div 
-          onClick={() => handleFavouritesClick(movie)}
+          onClick={() => props.handleFavouritesClick(movie)}
           className="overlay"
         >
          <FavouriteComponent />
